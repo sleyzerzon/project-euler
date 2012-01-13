18bcaedc177d474016385dd9a9a8c794a5822a1f
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2012-01-13 15:04:11 +0100

    Alléluia

--- problem-1/prob1.clj
+++ problem-1/prob1.clj
@@ -1,23 +1,7 @@
 (ns prob1)
-  
-(defn mod2[arg]
-  (mod 2 arg))
-  
-(defn isMod2[arg]
-  (= (mod2 arg) 0))
+    
+(defn isMod3or5[arg]
+  (or (zero? (mod arg 3)) (zero? (mod arg 5))))
 
-(defn mod5[arg]
-  (mod 5 arg))  
-
-(defn isMod5[arg]
-  (= (mod5 arg) 0))
-  
-(defn isMod2or5[arg]
-  (or (isMod2 arg) (isMod5 arg)))
-
-DOESN'T WORK :S
-(defn main [] 
-  (let [result 0]
-    (doseq [i (range 1 1000)]
-        (when (isMod2or5 i)
-          (println (+ result i))))))
+(defn calculation[]
+ (reduce + (filter isMod3or5 (range 1000))))
