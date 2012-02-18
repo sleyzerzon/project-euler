a507868efa3e7d2c582ef5fd3fc9ae5199a54a88
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2012-02-18 12:37:37 +0100

    resoled problem 9 in java

---
+++ problem-9/Prob9.java
@@ -0,0 +1,37 @@
+public class Prob9 {
+
+	public static void main(String[] args) {
+		System.out.println("The product abc of the only one Pythagorean triplet for which a + b + c = 1000 is "+ mainRoutine(1000));
+
+	}
+
+	public static int mainRoutine(int target) {
+		for (int a = 1; a < target; a++) {
+			for (int b = 1; b < target; b++) {
+				for (int c = 1; c < target; c++) {
+					if (isPythagoreanTripletAndSumForTarget(a, b, c, target)) {
+						return a * b * c;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+
+	public static boolean isPythagoreanTripletAndSumForTarget(int a, int b,	int c, int target) {
+		if (isPythagoreanTriplet(a, b, c) && (a + b + c == target)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean isPythagoreanTriplet(int a, int b, int c) {
+		if (a * a + b * b == c * c) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
